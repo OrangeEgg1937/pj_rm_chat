0ceaca001f029bab639dda0c1fabdefb76bbc024
OrangeEgg1937 <1155147738@link.cuhk.edu.hk>
2024-04-10 20:55:52 +0800

    Enhancement Voice changer and TTS

--- TextMessageHandler.py
+++ TextMessageHandler.py
@@ -5,6 +5,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtCore import Qt, QThread
 from PyQt5.QtWebSockets import QWebSocket
 from qasync import QEventLoop
+import pyttsx3
 
 from UI.Ui_mainWindow import Ui_MainWindow
 from connection.host import ChatroomHost
@@ -23,8 +24,18 @@ class TextMessageHandler:
         # register the signal
         self.ui.chat_input.returnPressed.connect(self.__onUserPressEnterInChat)
 
+        # register the signal
+        self.ui.ttsToggle.clicked.connect(self.__ttsToggleClicked)
+
         # register the header callback
         self.connectionHandler.connect_header_callback(ChatHeader.TEXT, self.__OnNewMessageComing)
+
+        self.ttsActive = False
+        self.isReading = False
+        self.readList = []
+
+        # start text to speech loop
+        asyncio.run_coroutine_threadsafe(self.__shouldIReadText(), self.connectionHandler.event_loop)
 
     def __onUserPressEnterInChat(self):
         # get the message from the UI
@@ -49,3 +60,39 @@ class TextMessageHandler:
         # add the message into the chat box
         message = data.data
         self.ui.chatbox.append(message)
+        if (self.ttsActive):
+            self.readList.append(message)
+
+    def __ttsToggleClicked(self):
+        if not self.ttsActive:
+            self.ttsActive = True
+        else:
+            self.ttsActive = False
+            self.readList.clear()
+
+    async def __shouldIReadText(self):
+        while True:
+            # Checks if there is new message to read
+            if (len(self.readList) != 0 and self.isReading == False):
+                task = asyncio.create_task(self.__readText())
+                await task
+            await asyncio.sleep(0.5)
+
+    async def __readText(self):
+        self.isReading = True
+        message = self.readList.pop()
+
+        # Initialize the converter
+        converter = pyttsx3.init()
+
+        # set text to speed properties
+        converter.setProperty('rate', 150)
+        converter.setProperty('volume', 0.7)
+
+        converter.say(message)
+        # Call blocking function with executor
+        await self.connectionHandler.event_loop.run_in_executor(None, self.__playText, converter)
+        self.isReading = False
+
+    def __playText(self, converter):
+        converter.runAndWait()
--- VoiceChatHandler.py
+++ VoiceChatHandler.py
@@ -10,6 +10,7 @@ import threading
 import time
 import pytz
 from datetime import datetime
+import wave
 
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtNetwork import QAbstractSocket
@@ -21,6 +22,7 @@ from ConnectionHandler import ConnectionHandler
 
 # define the sample rate
 SAMPLE_RATE = 44100
+swidth = 2
 
 
 class AudioPlayer:
@@ -58,9 +60,15 @@ class VoiceChatHandler:
         self.audioBuffer = queue.Queue()
 
         self._raw_audio_data = None
+        self.changePitch = False
+        self.pitchStrength = 0
 
         # register the signal
         self.ui.push_to_talk.clicked.connect(self.__start_speaking)
+
+        # register events to functions
+        self.ui.pitchToggle.clicked.connect(self.__onTogglePitch)
+        self.ui.PitchSlider.sliderMoved.connect(self.__updateSlider)
 
         # add the header callback
         self.connectionHandler.connect_header_callback(ChatHeader.AUDIO, self.__audio_data_in)
@@ -83,6 +91,9 @@ class VoiceChatHandler:
         if len(self.single_audio_data) >= SAMPLE_RATE * 2:
             # push the audio data to the buffer
             temp = self.single_audio_data
+            # Apply pitch change if wanted
+            if (self.changePitch):
+                temp = self.__changePitch(temp)
             # compress the audio data
             compressed_data = zlib.compress(temp)
             currentDateAndTime = datetime.now(pytz.utc)
@@ -122,3 +133,44 @@ class VoiceChatHandler:
         print("Received audio data at ", currentDateAndTime)
         # add the audio data to the buffer
         self._audio_player.audioBuffer.put(audio)
+
+    # Toggles between pitch and no pitch change
+    def __onTogglePitch(self):
+        if not self.changePitch:
+            self.changePitch = True
+        else:
+            self.changePitch = False
+
+    # Updates slider
+    def __updateSlider(self):
+        newValue = self.ui.PitchSlider.value()
+
+        self.ui.pitchValue.setText("Pitch: " + str(newValue))
+        self.pitchStrength = 2 * newValue
+
+    # Pitch changer
+    def __changePitch(self, audio_data):
+        audio_data = np.array(wave.struct.unpack("%dh" % (len(audio_data) / swidth), audio_data))
+
+        # do Fourier Transform
+        audio_data = np.fft.rfft(audio_data)
+
+        # Pitch shifting
+        audio_data2 = [0] * len(audio_data)
+        if self.pitchStrength >= 0:
+            audio_data2[self.pitchStrength:len(audio_data)] = audio_data[0:(len(audio_data) - self.pitchStrength)]
+            audio_data2[0:self.pitchStrength] = audio_data[(len(audio_data) - self.pitchStrength):len(audio_data)]
+        else:
+            audio_data2[0:(len(audio_data) + self.pitchStrength)] = audio_data[-self.pitchStrength:len(audio_data)]
+            audio_data2[(len(audio_data) + self.pitchStrength):len(audio_data)] = audio_data[0:-self.pitchStrength]
+
+        shifted_data = np.array(audio_data2)
+        # Done shifting
+
+        # Inverse transform
+        shifted_data = np.fft.irfft(shifted_data)
+
+        # Turn data back into buffer like object
+        dataout = shifted_data.astype(np.int16).tobytes()
+
+        return dataout
